bafd30a72d2c5a57903ef39f4802fae7d8707e17
Jack Chan <JackChanHW@connect.hku.hk>
2023-01-03 01:45:23 +0000

    Remove bad bench test

--- main_bench.ts
+++ main_bench.ts
@@ -1,11 +1,5 @@
 import { walkDir } from "./main.ts"
 
-Deno.bench(async function readCodeBase() {
-  await walkDir("/Users/jack/dev/jackchw", async (_filePath) => {
-    return
-  })
-})
-
 Deno.bench(async function readRoot() {
   await walkDir(".", async (_filePath) => {
     return
